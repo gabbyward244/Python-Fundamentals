229318bc814aab0fe3945d5e053ba353c695b511
Corin Chepko <cchepko2@coloradomesa.edu>
2023-02-10 16:55:12 -0700

    aseg

--- CS0Spring23/kattis/digitswap/digitswap.py
+++ CS0Spring23/kattis/digitswap/digitswap.py
@@ -5,15 +5,15 @@
 
 digits = input()
 
-r_digits = ""
-r_digits += digits[1]
-r_digits += digits[0]
+#r_digits = ""
+#r_digits += digits[1]
+#r_digits += digits[0]
 
-print(r_digits)
+#print(r_digits)
 
 #print( digits[0] )
 #print( digits[1] )
 
 #digits = digits[:]
 
-#print(digits[::-1])
+print(digits[::-1])
